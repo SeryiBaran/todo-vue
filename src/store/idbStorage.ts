24c7691d9d5db0ddef9bb6ec61c06a0b675ff7a7
SeryiBaran <seryibaranminepy@yandex.ru>
2023-01-25 16:07:56 +0300

    minimize code

--- src/store/idbStorage.ts
+++ src/store/idbStorage.ts
@@ -2,13 +2,7 @@ import * as idb from 'idb-keyval';
 
 // Адаптер IDB для pinia-plugin-persistedstate-2
 export const storage = {
-  getItem: async function (key: string) {
-    return idb.get(key);
-  },
-  setItem: async function (key: string, val: any) {
-    await idb.set(key, val);
-  },
-  removeItem: async function (key: string) {
-    await idb.del(key);
-  },
+  getItem: idb.get,
+  setItem: idb.set,
+  removeItem: idb.del,
 };
