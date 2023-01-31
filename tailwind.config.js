00f9677271992eb15fe07064b0f6bb306ac6743f
SeryiBaran <seryibaranminepy@yandex.ru>
2023-01-31 11:09:43 +0300

    daisyui theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#2563eb',
+          secondary: '#4f46e5',
+          accent: '#4b5563',
+          neutral: '#191D24',
+          'base-100': '#2A303C',
+          info: '#3ABFF8',
+          success: '#36D399',
+          warning: '#FBBD23',
+          error: '#F87272',
+        },
+      },
+    ],
+  },
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
   theme: {
     extend: {},
